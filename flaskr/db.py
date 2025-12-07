b8d092ae7b5b08864e21c775db00b81b9f420fde
N <persaudnirav@gmail.com>
2025-12-07 15:06:29 -0500

    chore: updated gitignore

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,5 +1,7 @@
 import psycopg2
+from psycopg2.extensions import cursor
 import os
+import datetime
 def init_db():
     conn = psycopg2.connect(
             host=os.getenv('DB_HOST'),
@@ -8,5 +10,13 @@ def init_db():
             password=os.getenv('DB_PASS')
         )
     cur = conn.cursor()
-    if cur:
-        print("db connected")
+    add_db(conn,cur)
+def add_db(conn,cur: cursor):
+    cur.execute(""" 
+                INSERT INTO files (id, dateof)
+                VALUES (%s, %s);
+                 """,
+                (1, datetime.date(2025,12,5)))
+    conn.commit()
+
+
